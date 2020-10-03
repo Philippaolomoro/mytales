155a31fa6c1c75a8bb5eb88254e913bec028afaf
Philippa Olomoro <olomoro.philippa@gmail.com>
2020-10-03 03:20:32 +0100

    updated the google callback url

--- config/passport.js
+++ config/passport.js
@@ -22,7 +22,7 @@ module.exports = function (passport) {
 			{
 				clientID: process.env.GOOGLE_CLIENT_ID,
 				clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-				callbackURL: "http://localhost:5000/auth/google/callback",
+				callbackURL: "https://git.heroku.com/my-tales.git/auth/google/callback",
 			},
 			async (accessToken, refreshToken, profile, done) => {
 				let newUser = {
